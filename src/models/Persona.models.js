9e44b15541b2027f977d1d331bdb2214cd524d0b
FANOR26 <87057752+Fanor26@users.noreply.github.com>
2022-07-13 08:41:23 +0000

    Backend Nodejs III

--- src/models/Persona.models.js
+++ src/models/Persona.models.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose') 
 const {Schema}= mongoose 
-const PrimeroasecSchema = new Schema({ 
-primernombre:String, 
-segundonombre:String,
-tercernombre:String,
-apellidopaterno:String,
-apellidomaterno:String,
 
 
-correomovil:String, 
-contrasena: String, 
+
+const VacunadosSchema = new Schema({ 
+nombres:{type:String, required: [true, 'Nombre Obiigatorio']} ,
+apellidos:String,
+ciudad:String,
+Correo:String,
+estadVacuna:String,
+dosisAplicadas:String,
+ciudadNombre:String,
 date:{type:Date,default:Date.now} 
 
 })
 //Convertir modelo 
-module.exports= mongoose.model('vacunados',PrimeroasecSchema) 
+module.exports= mongoose.model('primeroasec',VacunadosSchema) 
--- src/routes/Ciudad.route.js
+++ src/routes/Ciudad.route.js
@@ -3,5 +3,6 @@ const router = Router()
 const CiudadCtrl = require('../controller/Ciudad.controller')
 
 router.post('/crear',CiudadCtrl.crearCiudad)
+router.post('/login',CiudadCtrl.login)
 
 module.exports=router 
