d410b8ce3e8d1ef5aea734e36a528ab8653b4770
Fanor <fanorarmandochoque@gmai.com>
2023-04-10 23:27:39 -0400

    fanor184

--- src/database.js
+++ src/database.js
@@ -1,18 +1,16 @@
 //Creamos nuestra base de datos
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-URI = 'mongodb+srv://sistemita:fanoro@cluster0.j7bgf.mongodb.net/?retryWrites=true&w=majority';
+URI =
+  'mongodb://mongo:plpT9Qnq6OWBidG4M3Mg@containers-us-west-35.railway.app:5843';
 
-
-
-
-
-mongoose.connect(URI,{
+mongoose
+  .connect(URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  })
+  .then((db) => console.log('Base de datos conectado', db.connection.name))
+  .catch((error) => console.log(error));
 
-})
-.then (db => console.log('Base de datos conectado', db.connection.name)).catch(error => console.log(error))
-
-module.exports= mongoose
+module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 // Requerimos las dependencias necesarias
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const bodyparser = require("body-parser");
-require("./database");
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+require('./database');
 
 //Configuracion del puerto
 
-app.set("Port", process.env.PORT || 5000);
-app.use(morgan("dev"));
+app.set('Port', process.env.PORT || 8080);
+app.use(morgan('dev'));
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
-app.use(cors({ origen: "*" }));
+app.use(cors({ origen: '*' }));
 
-app.use("/ciudad", require("./routes/Ciudad.route"));
-app.use("/persona", require("./routes/Persona.route"));
-app.listen(app.get("Port"), () => {
-  console.log("Servidor escuchando por el puerto", app.get("Port"));
+app.use('/ciudad', require('./routes/Ciudad.route'));
+app.use('/persona', require('./routes/Persona.route'));
+app.listen(app.get('Port'), () => {
+  console.log('Servidor escuchando por el puerto', app.get('Port'));
 });
